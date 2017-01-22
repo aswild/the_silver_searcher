5828520506aa0888fb7560bdb3f3f0b4bbe8825e
Allen Wild <allenwild93@gmail.com>
2017-01-21 22:41:09 -0500

    build.sh: don't re-run autogen if not needed
    Pass --force or -f as the first arg to force running autogen.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $1 == -f ]] || [[ $1 == --force ]]; then
+    FORCE_AUTOGEN=y
+    shift
+fi
 
 set -e
 cd "$(dirname "$0")"
 
-./autogen.sh
+if [[ $FORCE_AUTOGEN == y ]] || [[ ! -f ./configure ]]; then
+    ./autogen.sh
+fi
 ./configure "$@"
 make -j4
