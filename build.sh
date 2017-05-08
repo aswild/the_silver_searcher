25e2ee45af3cdbed3a08b8ba7d247238e8c93692
Allen Wild <allenwild93@gmail.com>
2017-05-08 16:57:45 -0400

    Re-add build.sh (accidentally got deleted in the last commit)

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $1 == -f ]] || [[ $1 == --force ]]; then
+    FORCE_AUTOGEN=y
+    shift
+fi
+
+set -e
+cd "$(dirname "$0")"
+
+if [[ $FORCE_AUTOGEN == y ]] || [[ ! -f ./configure ]]; then
+    ./autogen.sh
+fi
+./configure "$@"
+make -j4
