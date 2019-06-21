437fbbaa330bb3eefea3c201c6d0770e9d74647d
Allen Wild <allenwild93@gmail.com>
2019-06-21 01:09:51 -0400

    initial basic libarchive support
    Rather than writing decompression routines by hand, libarchive can be
    used to automatically decompress a big list of compression formats.
    
    A new AG_LIBARCHIVE compression type is defined, and the libarchive raw
    format is used to decompress the file buffer.
    
    Benchmarking and profiling is needed, especially since this adds memory
    allocations and other expensive library calls to is_zipped, which is
    called on every file when the -z option is enabled. If libarchive
    new/open/free is too slow, maybe it can be replaced with standalone
    magic checks for its various formats, though that could get messy.

--- src/search.c
+++ src/search.c
@@ -470,6 +470,7 @@ void search_file(const char *file_full_path) {
 #endif
 
     if (opts.search_zip_files) {
+        log_debug("check if %s is compressed", file_full_path);
         ag_compression_type zip_type = is_zipped(buf, f_len);
         if (zip_type != AG_NO_COMPRESSION) {
             size_t _buf_len = f_len;
